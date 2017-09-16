03ebec411d4277503216c1910b1b176e89e74e26
Tejas Prajapati <prajapatitejas91@gmail.com>
2017-09-16 17:17:16 +0530

    SRC: updated the functionality.
    For better performance and smaller footprint.
    
    Signed-off-by: Tejas Prajapati <prajapatitejas91@gmail.com>

--- SRC/Page_Test/Page_Test.ino
+++ SRC/Page_Test/Page_Test.ino
@@ -4,7 +4,13 @@
 
 #define OFF_FILE "/off.html"
 #define ON_FILE "/on.html"
- 
+
+typedef enum{
+  OFF = 0,
+  ON,
+  ERR,
+}response_vals;
+
 const char* ssid = "Tejas";
 const char* password = "7041679923";
  
@@ -16,6 +22,9 @@ bool one_time_only = 1;
 File off_f;
 File on_f;
 int value = HIGH; /*  Pin on which LED is there is working in inverted form*/
+int response;
+String on_content;
+String off_content;
 
 WiFiServer server(80);
  
@@ -23,7 +32,7 @@ void setup() {
   Serial.begin(115200);
   delay(10);
 
-  // Start filing subsystem
+  /*  Start filing subsystem */
   if (SPIFFS.begin()) {
       Serial.println("SPIFFS Active");
       Serial.println();
@@ -35,7 +44,7 @@ void setup() {
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, HIGH);
  
-  // Connect to WiFi network
+   /* Connect to WiFi network */
   Serial.println();
   Serial.println();
   Serial.print("Connecting to ");
@@ -50,11 +59,11 @@ void setup() {
   Serial.println("");
   Serial.println("WiFi connected");
  
-  // Start the server
+  /*  Start the server */
   server.begin();
   Serial.println("Server started");
  
-  // Print the IP address
+  /* Print the IP address */
   Serial.print("Use this URL to connect: ");
   Serial.print("http://");
   Serial.print(WiFi.localIP());
@@ -62,81 +71,80 @@ void setup() {
 }
  
 void loop() {
-  // Check if a client has connected
+
+  /* Check if a client has connected*/
   WiFiClient client = server.available();
   if (!client) {
     return;
   }
  
-  // Wait until the client sends some data
+  /* Wait until the client sends some data */
   Serial.println("new client");
   while(!client.available()){
     delay(1);
   }
 
-  // Read the first line of the request
+  /* Read the first line of the request */
   String request = client.readStringUntil('\r');
   Serial.println(request);
   client.flush();
 
   if (spiffsActive && one_time_only) {
-      if (SPIFFS.exists(OFF_FILE) && SPIFFS.exists(ON_FILE) ) {
-        off_f = SPIFFS.open(OFF_FILE, "r");
-        on_f = SPIFFS.open(ON_FILE, "r");
-        if(!off_f || !on_f){
-          Serial.println("Failed to open a file");
-        } else {
-          String off_content;
-          while (off_f.position()<off_f.size())
-          {
-            off_content = off_f.readStringUntil('\n');
-            off_content.trim();
-            client.println(off_content);
-          } 
-          off_f.close();  
-        }
-      one_time_only = 0;
+    if (SPIFFS.exists(OFF_FILE) && SPIFFS.exists(ON_FILE) ) {
+        Serial.println("Files exists");
+    } else {
+        Serial.println("Files do not exist");
+        response = ERR;
     }
+    one_time_only = 0;
   }
-  // Match the request
-  if (request.indexOf("/LED=ON") != -1)  {
-    digitalWrite(ledPin, LOW);
-    value = LOW;
-    on_display = 1;
-    off_display = 0;
-  }
+  /* Match the request */
   if (request.indexOf("/LED=OFF") != -1)  {
     digitalWrite(ledPin, HIGH);
     value = HIGH;
-    on_display = 0;
-    off_display = 1;
+    response = OFF;
+  }
+  if (request.indexOf("/LED=ON") != -1)  {
+    digitalWrite(ledPin, LOW);
+    value = LOW;
+    response = ON;
   }
   
-  // Return the response
-  if(on_display){
-    on_f = SPIFFS.open(ON_FILE, "r");
-    String on_content;
-    while (on_f.position()<on_f.size())
-    {
-      on_content = on_f.readStringUntil('\n');
-      on_content.trim();
-      client.println(on_content);
-    } 
-    on_f.close();
-  } else if(off_display){
-    off_f = SPIFFS.open(OFF_FILE, "r");
-    String off_content;
-    while (off_f.position()<off_f.size())
-    {
-      off_content = off_f.readStringUntil('\n');
-      off_content.trim();
-      client.println(off_content);
-    } 
-    off_f.close();    
+  /*  Return the response */
+
+  switch(response){
+    case ON:
+
+      on_f = SPIFFS.open(ON_FILE, "r");
+      while (on_f.position()<on_f.size())
+      {
+        on_content = on_f.readStringUntil('\n');
+        on_content.trim();
+        client.println(on_content);
+      } 
+      on_f.close();
+      break;
+
+    case OFF:
+    default:
+
+      off_f = SPIFFS.open(OFF_FILE, "r");
+      while (off_f.position()<off_f.size())
+      {
+        off_content = off_f.readStringUntil('\n');
+        off_content.trim();
+        client.println(off_content);
+      } 
+      off_f.close();
+      break;
+
+    case ERR:
+
+      client.println("File not found ERR 404");
+      break;
   }
-  
+
   delay(1);
   Serial.println("Client disonnected");
   Serial.println("");
 }
-
