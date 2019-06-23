34145398a76f2335f5103ba0f9680a4acd980ac2
Tejas Prajapati <prajapatitejas91@gmail.com>
2019-06-23 10:14:29 +0530

    SRC: Added map function to identify the humidity
    Humidity with the percentage value can be identified easily
    with map() no other things are required. Need this to be connected
    to openhab.

--- SRC/Page_Test/Page_Test.ino
+++ SRC/Page_Test/Page_Test.ino
@@ -4,7 +4,7 @@
 
 #define OFF_FILE "/off.html"
 #define ON_FILE "/on.html"
-#define DEBUG 0
+#define DEBUG 1
 #define MAX_VALS  16
 
 #define DRY_AIR_LOW 0
@@ -16,14 +16,14 @@
 #define AIR_CALIBRATED  0
 
 typedef enum{
-  OFF = 0,
-  ON,
-  ERR,
+	OFF = 0,
+	ON,
+	ERR,
 }response_vals;
 
 typedef enum{
-  ONLY_WATER = 0,
-  HANDS_ON_SENSOR,
+	ONLY_WATER = 0,
+	HANDS_ON_SENSOR,
 }pump_vals;
 
 const char* ssid = "Tejas";
@@ -38,168 +38,154 @@ String on_content,off_content;
 unsigned int base_value, measure_value;
 int delta_value;
 int sensor_read;
+int soilMoisture;
+int Sensorval;
 
 WiFiServer server(80);
  
 void setup() {
-  Serial.begin(115200);
-  delay(10);
+	Serial.begin(115200);
+	delay(10);
 
-  /*  Start filing subsystem */
-  if (SPIFFS.begin()) {
-      Serial.println("SPIFFS Active");
-      Serial.println();
-      spiffsActive = true;
-  } else {
-      Serial.println("Unable to activate SPIFFS");
-  }
- 
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, HIGH);
- 
+	/*  Start filing subsystem */
+	if (SPIFFS.begin()) {
+		Serial.println("SPIFFS Active");
+		Serial.println();
+		spiffsActive = true;
+	} else {
+		Serial.println("Unable to activate SPIFFS");
+	}
+
+	pinMode(ledPin, OUTPUT);
+	digitalWrite(ledPin, HIGH);
+
    /* Connect to WiFi network */
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+	Serial.println();
+	Serial.println();
+	Serial.print("Connecting to ");
+	Serial.println(ssid);
+
+	WiFi.begin(ssid, password);
  
-  WiFi.begin(ssid, password);
- 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
- 
-  /*  Start the server */
-  server.begin();
-  Serial.println("Server started");
- 
-  /* Print the IP address */
-  Serial.print("Use this URL to connect: ");
-  Serial.print("http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("/");
-  base_value = Average_adc();
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		Serial.print(".");
+	}
+	Serial.println("");
+	Serial.println("WiFi connected");
+
+	/*  Start the server */
+	server.begin();
+	Serial.println("Server started");
+
+	/* Print the IP address */
+	Serial.print("Use this URL to connect: ");
+	Serial.print("http://");
+	Serial.print(WiFi.localIP());
+	Serial.println("/");
 }
  
 void loop() {
 
-  Serial.print("Averag_Diff: ");
-  Serial.println(Average_Diff());
+	Sensorval = Read_adc();
+	soilMoisture = map(Sensorval,1024,0,0,100);
 
-  if(delta_value >= 0)
-  {
-    switch(delta_value)
-    {
-      case DRY_AIR_LOW ... DRY_AIR_HIGH:
-        #if AIR_CALIBRATED
-        Serial.println("Dry Air");
-        #elif WATER_CALIBRATED
-        Serial.println("Only Water");
-        #endif
-        sensor_read = ONLY_WATER;
-        break;
-      case HANDS_LOW ... HANDS_HIGH:
-        Serial.println("Hands on Sensor");
-        sensor_read = HANDS_ON_SENSOR;
-    }
-  }
-  delay(1000);
+	Serial.println("soilMoisture:");
+	Serial.println(soilMoisture, DEC);
+	delay(1000);
 #if 1
+
+	/* Check if a client has connected*/
+	WiFiClient client = server.available();
+	if (!client) {
+		return;
+	}
+
+	/* Wait until the client sends some data */
+	Serial.println("new client");
+	while(!client.available()){
+		delay(1);
+	}
+
+	/* Read the first line of the request */
+	String request = client.readStringUntil('\r');
+	Serial.println(request);
+	client.flush();
+
+	if (spiffsActive && one_time_only) {
+		if (SPIFFS.exists(OFF_FILE) && SPIFFS.exists(ON_FILE) ) {
+			Serial.println("Files exists");
+		} else {
+			Serial.println("Files do not exist");
+			response = ERR;
+		}
+		one_time_only = 0;
+	}
+	/* Match the request */
+	if (request.indexOf("/LED=OFF") != -1)  {
+		digitalWrite(ledPin, HIGH);
+		value = HIGH;
+		response = OFF;
+	}
+	if (request.indexOf("/LED=ON") != -1)  {
+		digitalWrite(ledPin, LOW);
+		value = LOW;
+		response = ON;
+	}
   
-  /* Check if a client has connected*/
-  WiFiClient client = server.available();
-  if (!client) {
-    return;
-  }
- 
-  /* Wait until the client sends some data */
-  Serial.println("new client");
-  while(!client.available()){
-    delay(1);
-  }
+	/*  Return the response */
+  /* TODO: Put a value in condition for the range that can decide
+   * when turn on or turn off
+   */
+	switch(response){
+		case ON:
+			on_f = SPIFFS.open(ON_FILE, "r");
+			while (on_f.position()<on_f.size()) {
+				on_content = on_f.readStringUntil('\n');
+				on_content.trim();
+				client.println(on_content);
+				if(on_content.substring(0) == "<html>"){
+					if(sensor_read == ONLY_WATER){
+					client.println("Only water, pump would be turned off\n");
+				} else if(sensor_read == HANDS_ON_SENSOR) {
+					client.println("Pour some water\n");
+				} else {
+					client.println("Keep the pump off so no water wastage\n");
+				}
+			}
+		}
+			on_f.close();
+		break;
 
-  /* Read the first line of the request */
-  String request = client.readStringUntil('\r');
-  Serial.println(request);
-  client.flush();
+		case OFF:
+		default:
+			off_f = SPIFFS.open(OFF_FILE, "r");
+			while (off_f.position()<off_f.size()) {
+				off_content = off_f.readStringUntil('\n');
+				off_content.trim();
+				client.println(off_content);
+				if(off_content.substring(0) == "<html>"){
+					if(sensor_read == ONLY_WATER){
+						client.println("Only water, pump would be turned off\n");
+					} else if(sensor_read == HANDS_ON_SENSOR) {
+						client.println("Pour some water\n");
+					} else {
+						client.println("Keep the pump off so no water wastage\n");
+					}
+				}
+			}
+			off_f.close();
+		break;
 
-  if (spiffsActive && one_time_only) {
-    if (SPIFFS.exists(OFF_FILE) && SPIFFS.exists(ON_FILE) ) {
-        Serial.println("Files exists");
-    } else {
-        Serial.println("Files do not exist");
-        response = ERR;
-    }
-    one_time_only = 0;
-  }
-  /* Match the request */
-  if (request.indexOf("/LED=OFF") != -1)  {
-    digitalWrite(ledPin, HIGH);
-    value = HIGH;
-    response = OFF;
-  }
-  if (request.indexOf("/LED=ON") != -1)  {
-    digitalWrite(ledPin, LOW);
-    value = LOW;
-    response = ON;
-  }
-  
-  /*  Return the response */
+		case ERR:
 
-  switch(response){
-    case ON:
-      on_f = SPIFFS.open(ON_FILE, "r");
-      while (on_f.position()<on_f.size())
-      {
-        on_content = on_f.readStringUntil('\n');
-        on_content.trim();
-        client.println(on_content);
-        if(on_content.substring(0) == "<html>"){
-          if(sensor_read == ONLY_WATER){
-              client.println("Only water, pump would be turned off\n");
-          } else if(sensor_read == HANDS_ON_SENSOR) {
-              client.println("Pour some water\n");
-          } else {
-              client.println("Keep the pump off so no water wastage\n");
-          }
-        }
-      }
-      on_f.close();
-      break;
+			client.println("File not found ERR 404");
+		break;
+	}
 
-    case OFF:
-    default:
-      off_f = SPIFFS.open(OFF_FILE, "r");
-      while (off_f.position()<off_f.size())
-      {
-        off_content = off_f.readStringUntil('\n');
-        off_content.trim();
-        client.println(off_content);
-        if(off_content.substring(0) == "<html>"){
-          if(sensor_read == ONLY_WATER){
-              client.println("Only water, pump would be turned off\n");
-          } else if(sensor_read == HANDS_ON_SENSOR) {
-              client.println("Pour some water\n");
-          } else {
-              client.println("Keep the pump off so no water wastage\n");
-          }
-        }
-      } 
-      off_f.close();
-      break;
-
-    case ERR:
-
-      client.println("File not found ERR 404");
-      break;
-  }
-
-  delay(1);
-  Serial.println("Client disonnected");
-  Serial.println("");
+	delay(1);
+	Serial.println("Client disonnected");
+	Serial.println("");
 #endif
 }
 
@@ -207,56 +193,7 @@ void loop() {
 int Read_adc()
 {
 #if DEBUG
-  Serial.println(analogRead(A0));  
+	Serial.println(analogRead(A0));
 #endif  
-  return analogRead(A0);
+	return analogRead(A0);
 }
-
-int Average_adc()
-{
-  int vals[MAX_VALS],i, Final_Val = 0;
-  for(i = 0; i < MAX_VALS; i++)
-  {
-    vals[i] = Read_adc();
-  }
-
-  for(i = 0; i < MAX_VALS; i++)
-  {
-    Final_Val += vals[i];
-  }
-
-  Final_Val >>= (int)(log10(MAX_VALS) / log10(2));
-  return Final_Val;
-}
-
-int Diff_Reading()
-{
-  measure_value = Average_adc();
-
-  delta_value = base_value - measure_value;
-  
-  if(delta_value < 0)
-  {
-    base_value = (base_value + measure_value) >> 1;
-    delta_value = 0;
-  }
-  return delta_value;
-}
-
-int Average_Diff()
-{
-  int vals[MAX_VALS],i, Final_Val = 0;
-  for(i = 0; i < MAX_VALS; i++)
-  {
-    vals[i] = Diff_Reading();
-  }
-
-  for(i = 0; i < MAX_VALS; i++)
-  {
-    Final_Val += vals[i];
-  }
-
-  Final_Val >>= (int)(log10(MAX_VALS) / log10(2));
-  return Final_Val;  
-}
-
